2f8eaeea1e8d0c7bed5d2088134541469560d280
Norman Huth <norman@huth.it>
2021-02-20 07:29:39 +0100

    Add Github URL

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -27,7 +27,7 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: '',
+    repo: 'Zitkala/Hallo.Tools-Docs',
     editLinks: true,
     docsDir: 'src',
     docsRepo:'Zitkala/Hallo.Tools-Docs',
